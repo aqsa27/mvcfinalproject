61a081ffc57f27ad5aafef6de5289ce5ca94d01b
Aqsa R Sheikh <ars92@afsaccess1.njit.edu>
2017-12-11 20:30:20 -0500

    second changes

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 
-define('DATABASE', 'sd686');
-define('USERNAME', 'sd686');
-define('PASSWORD', 'ZOm1EN5l3');
-define('CONNECTION', 'sql1.njit.edu');
+define('DATABASE', 'ars92');
+define('USERNAME', 'ars92');
+define('PASSWORD', 'aYzx7BVp2yeDtxLJ');
+define('CONNECTION', 'sql2.njit.edu');
 
 //Autoloader class
 class Manage {
